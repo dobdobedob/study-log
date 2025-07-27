8f37531892616d453d73bea68fdcd5651afdccd4
dobdobedob <smkdkd1993@gmail.com>
2025-07-27 19:17:01 +0900

    전투분석 페이지 예시 작성

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="black-nav">
+        <div>도비 Blog</div>
+      </div>
+      <button className="btn btn-info" value={"딜러"}>
+        딜러
+      </button>
+      <button className="btn btn-info" value={"서폿"}>
+        서폿
+      </button>
+      <div>
+        닉네임 :
+        <input type="text" />
+      </div>
+      <div>
+        관문 :
+        <select>
+          <option value="dif">난이도</option>
+          <option value="normal">노말</option>
+          <option value="hard">하드</option>
+        </select>
+        <select>
+          <option value="act">장막</option>
+          <option value="prev">서막</option>
+          <option value="act1">1막</option>
+          <option value="act2">2막</option>
+          <option value="act3">3막</option>
+          <option value="act4">4막</option>
+          <option value="final">종막</option>
+        </select>
+        <select>
+          <option value="gate">관문</option>
+          <option value="gate1">1관</option>
+          <option value="gate2">2관</option>
+          <option value="gate3">3관</option>
+        </select>
+      </div>
+      <div>
+        전투시간 :
+        <input type="text" />
+      </div>
+      <div>
+        시너지 유효율 :
+        <input type="text" />
+      </div>
+      <div>
+        초당 피해량 :
+        <input type="text" />
+      </div>
+      <div>
+        <button>
+          입력
+        </button>
+      </div>
+    </div >
   );
 }
 
